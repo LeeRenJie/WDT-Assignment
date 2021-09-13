e92c66c07cd9c981f91669fa38564a483856aa1e
Lee Ren Jie <work.renjie@gmail.com>
2021-09-13 15:59:42 +0800

    nav update

--- frontend/public/pages/customer/home.php
+++ frontend/public/pages/customer/home.php
@@ -5,44 +5,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <link rel="stylesheet" href="../../../src/stylesheets/nav.css">
   <title>Home Page</title>
 </head>
 <body>
-<?php include '../shared/navbar.php';?>
+  <?php include '../shared/navbar.php';?>
 
-<?php include '../shared/footer.php';?>
-
-<!-- Jquery needed -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="js/scripts.js"></script>
-
-<!-- Bootstrap needed-->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-
-<!-- Icon -->
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-
-<!-- Function used to shrink nav bar removing paddings and adding black background -->
-<script>
-    $(window).scroll(function() {
-        if ($(document).scrollTop() > 50) {
-            $('.nav').addClass('affix');
-            $(".logo").attr("src", "../../images/logo-svg.svg");
-            // console.log("OK");
-        } else {
-            $('.nav').removeClass('affix');
-            $(".logo").attr("src", "../../images/transparent-logo-svg.svg");
-        }
-    });
-
-    $('.navTrigger').click(function () {
-      $(this).toggleClass('active');
-      console.log("Clicked menu");
-      $("#mainListDiv").toggleClass("show-list");
-      $("#mainListDiv").fadeIn();
-    });
+  <?php include '../shared/footer.php';?>
 </script>
 </body>
 </html>
