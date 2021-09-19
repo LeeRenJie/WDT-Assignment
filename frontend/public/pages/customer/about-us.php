482e5ccf18c7e404fe2432f0eb97a68364a7cb33
Lee Ren Jie <work.renjie@gmail.com>
2021-09-20 00:30:27 +0800

    part of about us page

--- frontend/public/pages/customer/about-us.php
+++ frontend/public/pages/customer/about-us.php
@@ -10,7 +10,18 @@
 <body>
   <?php include '../shared/navbar.php';?>
   <div class="container-fluid">
-    <p>About Us</p>
+    <div class="container pt-4">
+      <div class="row">
+        <div class="col-6">
+          <h1 class="mb-4">About Exclusive Pet Mart</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse suscipit, ligula et pharetra bibendum, orci diam condimentum nisl, at convallis massa neque ac neque. Proin vel ullamcorper lorem. Nullam feugiat consectetur nisl, quis rhoncus nibh dignissim non. Proin scelerisque augue pulvinar venenatis fringilla. Aenean posuere consequat rutrum. Maecenas placerat eros rutrum hendrerit auctor. Fusce quam ex, ornare et ex vitae, semper commodo nulla.</p>
+        </div>
+        <div class="col-6">
+          <img class="rounded float-right mr-4" src="../../images/logo-png.png" alt="" style="width:250px;height:250px;">
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#DEBA9D" fill-opacity="1" d="M0,128L48,149.3C96,171,192,213,288,224C384,235,480,213,576,197.3C672,181,768,171,864,144C960,117,1056,75,1152,48C1248,21,1344,11,1392,5.3L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
   </div>
   <?php include '../shared/footer.php';?>
 </script>
