d7330b60d63dc827a7497b7f752bce8a02203dff
Lee Ren Jie <work.renjie@gmail.com>
2021-09-19 16:34:51 +0800

    add file and edit footer

--- frontend/public/pages/customer/T&C.php
+++ frontend/public/pages/customer/T&C.php
@@ -13,8 +13,6 @@
     </head>
     <body>
     <?php include '../shared/navbar.php';?>
-
-    <?php include '../shared/footer.php';?>
         <!--
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
@@ -51,19 +49,20 @@
             <p>The Exclusive Pet Mart consists of a desktop and mobile web application that provides a hybrid peer-to-peer marketplace for pet-related services and products.
 
                 With one exception (stated in the following paragraph), Petstore.com is not a Service Provider or Product Seller. Petstore.com is a peer-to-peer marketplace for persons seeking pet products and services (“Purchaser”), persons offering pet services (“Service Provider”), and persons selling pet products (“Product Seller”) to communicate and arrange for the purchase and provision of pet goods and services. We make no representations or warranties about the quality or legality of pet products or services offered on the marketplace by Service Providers or Product Sellers. Petstore.com does not employ, recommend, or endorse any Product Sellers or Service Providers, nor will Petstore.com be responsible or liable for the conduct, quality, performance, or safety of any Service Provider, Product Seller, or the products and services they sell. While Petstore.com may perform a background check or offer guidance to Service Providers or Product Sellers, We make no claims or guarantees about the products and services offered, or of those who offer them. You are solely responsible for your personal safety and the safety of your pet(s).
-                
+
                 As a Hybrid peer-to-peer marketplace, Petstore.com does offer some commercial products for sale under the user account name Petstore with the email sales@petstore.com. For these products only, Petstore.com is a Product Seller. For these products only, Petstore.com offers a return for refund or exchange on new and unused products within 60 days of the date your order shipped. Beyond 60 days, Purchasers must contact the manufacturer directly for warranty service. For all products not sold under the user account name Petstore, you must contact the Product Seller regarding any product issues.
-                
+
                 All transactions (with the exception of products sold by the user account Petstore) are between Purchasers and Service Providers or Product Sellers. Our Service is to provide a marketplace and facilitate payment. Your use of the Services is agreement that We have no liability or responsibility for damages, to you or your pet, resulting from any use or misuse of our Services. You are responsible for determining the quality, safety, and suitability of the service or product offered. Although We offer a system for the review of Product Sellers and Service Providers, We make no claim that any reviews are accurate.
-                
-                Purchasers are solely responsible for providing Service Providers with emergency medical contacts for the care of their pet as well as personal contact information for the duration of the service. In case of emergency, if you (the Purchaser) are not available to obtain immediate care, you (the Purchaser) authorize Us and the Service Provider to obtain and provision veterinary care. If We obtain emergency veterinary care for your pet, you authorize your veterinarian to release your pet records to Us. If you refuse any veterinary care suggested by the Service Provider or Us, you release the Service Provider and Us for any injury, damage, or liability arising from failure to seek veterinary care. Purchasers are solely responsible for cost and payment of any emergency medical care. In any case where emergency services may be required due to any fault or negligence on the part of the Service Provider, Purchasers are solely responsible for seeking any remuneration or damages directly from the Service Provider. 
-                
+
+                Purchasers are solely responsible for providing Service Providers with emergency medical contacts for the care of their pet as well as personal contact information for the duration of the service. In case of emergency, if you (the Purchaser) are not available to obtain immediate care, you (the Purchaser) authorize Us and the Service Provider to obtain and provision veterinary care. If We obtain emergency veterinary care for your pet, you authorize your veterinarian to release your pet records to Us. If you refuse any veterinary care suggested by the Service Provider or Us, you release the Service Provider and Us for any injury, damage, or liability arising from failure to seek veterinary care. Purchasers are solely responsible for cost and payment of any emergency medical care. In any case where emergency services may be required due to any fault or negligence on the part of the Service Provider, Purchasers are solely responsible for seeking any remuneration or damages directly from the Service Provider.
+
                 Purchasers may request a service or product using the Services. When you request a service or product, you authorize Us to charge your credit card for the requested product or service. In requesting a product or service, you agree to pay the cost, plus fees and shipping. If you have questions about a product or service but do not want to make a transaction, you agree instead to contact the seller using the Services.
-                
+
                 Purchasers of pet services may only seek services for legally owned, vaccinated, and appropriately permitted pets in accordance with all local, state, and federal laws.</p>
             </div>
             <div></div>
         </div>
+        <?php include '../shared/footer.php';?>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
