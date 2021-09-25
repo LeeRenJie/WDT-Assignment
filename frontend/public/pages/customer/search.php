703d1349b76fc94005ab00ce251ceac4623820a0
Lee Ren Jie <work.renjie@gmail.com>
2021-09-25 11:21:40 +0800

    search page

--- frontend/public/pages/customer/search.php
+++ frontend/public/pages/customer/search.php
@@ -10,12 +10,66 @@
 </head>
 <body>
   <?php include '../shared/navbar.php';?>
-  <div class= "container">
-        <i class="fas fa-search"></i>
-        <input type="text" placeholder="Search.." name="search">
-        <button type="submit">Search</button>
+  <div class="container-fluid">
+    <div class= "container">
+      <i class="fas fa-search"></i>
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit">Search</button>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-2">
+          <ul>
+            <li>
+              <input id="c1" type="checkbox">
+              <label for="c1">Cat</label>
+            </li>
+            <li>
+              <input id="c2" type="checkbox">
+              <label for="c2">Dog</label>
+            </li>
+          </ul>
+        </div>
+        <div class="col-2">
+          <ul>
+            <li>
+              <input id="c3" type="checkbox">
+              <label for="c3">Toys</label>
+            </li>
+            <li>
+              <input id="c4" type="checkbox">
+              <label for="c4">Food</label>
+            </li>
+            <li>
+              <input id="c5" type="checkbox">
+              <label for="c5">Healthcare</label>
+            </li>
+            <li>
+              <input id="c6" type="checkbox">
+              <label for="c6">Gears</label>
+            </li>
+          </ul>
+        </div>
+        <div class="col-2">
+          <ul>
+              <li>
+                <input id="c7" type="checkbox">
+                <label for="c7">Least Expensive</label>
+              </li>
+              <li>
+                <input id="c8" type="checkbox">
+                <label for="c8">Most Expensive</label>
+              </li>
+            </ul>
+        </div>
+        <div class="col-3">
+
+        </div>
+      </div>
+    </div>
   </div>
   <?php include '../shared/footer.php';?>
+  <script src="search.js"></script>
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </script>
