0970220db38c9ddc35580cdc2dc7d2cde6b72763
Jh0247 <zikan111@gmail.com>
2021-10-16 16:58:40 +0800

    nav

--- frontend/public/pages/customer/edit-profile.php
+++ frontend/public/pages/customer/edit-profile.php
@@ -81,14 +81,6 @@
       </div>
     </div>
     <?php include '../shared/footer.php';?>
-    <script>
-    function togglepopup() {
-      document.getElementById("popform").style.display = "block";
-    }
-    function closeForm() {
-      document.getElementById("popform").style.display = "none";
-    }
-    </script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
   </body>
