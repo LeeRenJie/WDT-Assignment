c3238dceed068cf2116cdfadd2060b481dbe1c93
Lee Ren Jie <work.renjie@gmail.com>
2021-10-23 01:07:11 +0800

    update navbar link

--- frontend/public/pages/shared/navbar.php
+++ frontend/public/pages/shared/navbar.php
@@ -35,7 +35,7 @@
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link login-btn" href="../customer/login.php">Login</a>
+                <a class="nav-link login-btn" href="login.php">Login</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
